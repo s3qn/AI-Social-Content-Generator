670983b76ebe9a552e647cd965291dd08a81d0f3
Sean <sean@cheskis.com>
2026-05-07 14:12:32 +0000

    added working handle parser, and seperated code into functions

--- src/ai_social_content_generator/analytics/obtain-handle.py
+++ src/ai_social_content_generator/analytics/obtain-handle.py
@@ -3,45 +3,54 @@ from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 import os
 import json
+import argparse
 
 
-load_dotenv(Path("/home/sean/ai-projects/AI-Social-Content-Generator/.env"))
-apify_api_key = os.getenv("APIFY_API_KEY")
+def load_apify_api_key():
+    load_dotenv(find_dotenv())
+    apify_api_key = os.getenv("APIFY_API_KEY")
+    return apify_api_key
 
-def convert_to_json(data):
-    with open(Path("/home/sean/ai-projects/AI-Social-Content-Generator/cache/test.json"), "w") as f:
+def convert_to_json(data, handle):
+    with open(Path(__file__).resolve().parents[3] / "cache" / f"{handle}-posts.json", "w") as f:
         json.dump(data, f, indent=2, default=str)
     return None
 
-def main():
-    
+def fetch_handle(apify_api_key, handle):
+
     client = ApifyClient(apify_api_key)
     actor_client = client.actor('apify/instagram-scraper')
     result = actor_client.call(run_input={
-        "directUrls": ["https://www.instagram.com/inna_cheskis/"],
+        "directUrls": [f"https://www.instagram.com/{handle}/"],
         "resultsType": "posts",
-        "resultsLimit": 1,                       
+        "resultsLimit": 1,
     })
 
     if result is None:
         print('Actor run failed...')
         return
-    
-    print(f"Result keys: {result.keys()}")
+
+    # FOR DEBUG: print(f"Result keys: {result.keys()}")
 
     data = client.dataset(result["defaultDatasetId"])
     data_list = data.list_items().items
-    data_json = convert_to_json(data_list)
+    return data_list
 
+def main():
+    
+    parser = argparse.ArgumentParser(description="Fetch instagram posts from handle")
+    parser.add_argument("--user", help="Instagram Handle to fetch (e.g: nasa)" ,default="inna_cheskis")
+    args = parser.parse_args()
+    user = args.user
 
+    # Load API Key
+    apify_api_key = load_apify_api_key()
+    
+    # Scrape handle from the gram
+    data = fetch_handle(apify_api_key, user)
+    convert_to_json(data, user)
 
-
-
-    print(f'Dataset: {data_json}')
-
-
-
-
+    print(f'Dataset: {data}')
 
 if __name__ == "__main__":
     main()
